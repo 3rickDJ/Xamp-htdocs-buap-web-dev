d775177eaa113fa47403f411f7e2bb5caee47036
3rickDJ <erick@makingdevs.com>
2024-01-12 17:51:52 -0600

    mickey #1

---
+++ 3rick/mickey.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mickey</title>
+
+    <style>
+    .cuerpo {
+        width: 150px;
+        height: 150px;
+        background-color: teal;
+    }
+    .izquierda {
+        width: 50px;
+        height: 50px;
+        background-color: red;
+    }
+    .derecha {
+        width: 50px;
+        height: 50px;
+        background-color: blue;
+    }
+    .cara {
+        width: 100px;
+        height: 100px;
+        background-color: black;
+
+    }
+    </style>
+</head>
+<body>
+    <div class="cuerpo">
+        <div class="izquierda"> </div>
+        <div class="derecha"> </div>
+        <div class="cara"> </div>
+    </div>
+</body>
+</html>

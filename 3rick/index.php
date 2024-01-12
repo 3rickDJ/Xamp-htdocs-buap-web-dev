6d1d9b0cefc946729862649ba5ec5df2ce8a41c7
3rickDJ <erick@makingdevs.com>
2024-01-12 08:41:13 -0600

    add cartas clow

---
+++ 3rick/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cartas Clow</title>
+
+    <style>
+    .image {
+        max-width: 180px;
+        height: auto;
+        background-color: red;
+        margin: 3px;
+    }
+    </style>
+</head>
+<body>
+    <div>
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+    </div>
+    <div>
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+    </div>
+    <div>
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+    </div>
+    <div>
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+    </div>
+    <div>
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+        <img src="img/carta.jpeg" class="image" alt="">
+    </div>
+</body>
+</html>

87646d78a535541ce5b7013c8c4176eb7adce226
3rickDJ <erick@makingdevs.com>
2024-01-12 18:08:52 -0600

    mickey update

--- 3rick/mickey.php
+++ 3rick/mickey.php
@@ -1,39 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mickey</title>
 
     <style>
-    .cuerpo {
-        width: 150px;
-        height: 150px;
-        background-color: teal;
-    }
-    .izquierda {
-        width: 50px;
-        height: 50px;
-        background-color: red;
-    }
-    .derecha {
-        width: 50px;
-        height: 50px;
-        background-color: blue;
-    }
-    .cara {
-        width: 100px;
-        height: 100px;
-        background-color: black;
+        .cuerpo {
+            width: 150px;
+            height: 150px;
+            background-color: teal;
+            position: relative;
+        }
 
-    }
+        .izquierda {
+            width: 50px;
+            height: 50px;
+            background-color: black;
+            border-radius: 50%;
+            position: absolute;
+        }
+
+        .derecha {
+            width: 50px;
+            height: 50px;
+            background-color: black;
+            border-radius: 50%;
+            position: absolute;
+            left: 100px;
+        }
+
+        .cara {
+            width: 100px;
+            height: 100px;
+            background-color: black;
+            border-radius: 50%;
+            position: absolute;
+            top: 30px;
+            left: 25px;
+        }
     </style>
 </head>
+
 <body>
     <div class="cuerpo">
         <div class="izquierda"> </div>
         <div class="derecha"> </div>
         <div class="cara"> </div>
     </div>
+    <div class="cuerpo">
+        <div class="izquierda"> </div>
+        <div class="derecha"> </div>
+        <div class="cara"> </div>
+    </div>
 </body>
+
 </html>

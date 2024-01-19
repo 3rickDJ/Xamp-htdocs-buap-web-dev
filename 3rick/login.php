217959be2a0bf6d291855299dd7b729d5358a960
3rickDJ <erick@makingdevs.com>
2024-01-19 17:50:13 -0600

    feat: login.php

--- 3rick/login.php
+++ 3rick/login.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
         <div class="row">
@@ -16,8 +18,20 @@
             <div class="col-4 col-sm-4 col-md-2">5</div>
             <div class="col-4 col-sm-4 col-md-2">6</div>
         </div>
-
     </div>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-4">1</div>
+            <div class="col-12 col-sm-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5>Login</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-4">3</div>
+        </div>
+    </div>
 </body>
+
 </html>

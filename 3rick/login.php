edaa42fe6a11b8cc18255af88f4d609b9210c7df
3rickDJ <erick@makingdevs.com>
2024-01-19 17:43:06 -0600

    add: login.php

---
+++ 3rick/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-4 col-sm-4 col-md-2">1</div>
+            <div class="col-4 col-sm-4 col-md-2">2</div>
+            <div class="col-4 col-sm-4 col-md-2">3</div>
+            <div class="col-4 col-sm-4 col-md-2">4</div>
+            <div class="col-4 col-sm-4 col-md-2">5</div>
+            <div class="col-4 col-sm-4 col-md-2">6</div>
+        </div>
+
+    </div>
+    
+</body>
+</html>

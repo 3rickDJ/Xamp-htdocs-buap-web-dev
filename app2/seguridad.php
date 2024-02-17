9deb152c5bd3bdce36148ce5c561ce08b02043c9
3rickDJ <erick@makingdevs.com>
2024-02-16 18:06:51 -0600

    git modulo disque de seguridad jaja

---
+++ app2/seguridad.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$user = isset($_SESSION['user']) ? $_SESSION['user'] : '';
+$roll = isset($_SESSION['roll']) ? $_SESSION['roll'] : '';
+
+if ($user == "admin"){
+    echo "Bienvenido $user";
+    echo "<br>Eres un $roll";
+} else {
+    header("Location: login.php");
+}
+?>

1b5721c84ffbf8947811a67ca9e45ee9cbaf652c
3rickDJ <erick@makingdevs.com>
2024-02-16 18:15:28 -0600

    add index and show seguridad

---
+++ app2/index.php
@@ -0,0 +1,22 @@
+<?php
+include "seguridad.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1><?php echo $user; ?></h1>
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- app2/seguridad.php
+++ app2/seguridad.php
@@ -3,9 +3,6 @@ session_start();
 $user = isset($_SESSION['user']) ? $_SESSION['user'] : '';
 $roll = isset($_SESSION['roll']) ? $_SESSION['roll'] : '';
 
-if ($user == "admin"){
-    echo "Bienvenido $user";
-    echo "<br>Eres un $roll";
-} else {
+if ($roll != "admin"){
     header("Location: login.php");
 }

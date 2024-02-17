d5297d3e1d57c4b7a42343b6bd35b8daf2a8ce4b
3rickDJ <erick@makingdevs.com>
2024-02-16 18:39:20 -0600

    add: salir.php and button to logout

--- app2/index.php
+++ app2/index.php
@@ -4,19 +4,26 @@ include "seguridad.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
+
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <h1><?php echo $user; ?></h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1><?php echo $user; ?></h1>
+                <br>
+            </div>
+            <div class="col text-end">
+                <a href="salir.php" class="btn btn-danger">Salir</a>
+            </div>
         </div>
     </div>
-</div>
 </body>
+
 </html>
